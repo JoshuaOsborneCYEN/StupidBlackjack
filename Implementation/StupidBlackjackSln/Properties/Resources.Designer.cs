db5db15aa8f905c95d1f03bd4ff3b8e73d048145
ajbradberry96 <ajbradberry96@gmail.com>
2018-11-08 17:00:35 -0600

    Added background image to new game form.

--- Implementation/StupidBlackjackSln/Properties/Resources.Designer.cs
+++ Implementation/StupidBlackjackSln/Properties/Resources.Designer.cs
@@ -509,6 +509,16 @@ namespace StupidBlackjackSln.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap felt_table {
+            get {
+                object obj = ResourceManager.GetObject("felt_table", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream game_music {
